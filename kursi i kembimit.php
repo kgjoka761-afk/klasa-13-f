2f22fc9d11b329b82fbaf31e56a3270517659feb
kgjoka761-afk <kgjoka761@gmail.com>
2025-10-23 10:04:14 +0200

    version i ri me monedhat

--- kursi i kembimit.php
+++ kursi i kembimit.php
@@ -1,15 +1,21 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $euro = $_POST['euro'];
-    $kursi = $_POST['kursi'];
 
-    if (is_numeric($euro) && is_numeric($kursi)) {
-        $lek = $euro * $kursi;
-        echo "<h2>Shuma e konvertuar është: " . number_format($lek, 2) . " Lek (ALL)</h2>";
+    $monedha = $_POST['monedha'];   
+    $vlera   = $_POST['vlera'];     
+    $kursi   = $_POST['kursi'];     
+    if (is_numeric($vlera) && is_numeric($kursi)) {
+
+        $lek = $vlera * $kursi;
+
+        echo "<h2>Shuma e konvertuar (${monedha} → LEK) është: "
+             . number_format($lek, 2) . " Lek (ALL)</h2>";
+
     } else {
         echo "<h2>Ju lutem shkruani vetëm numra!</h2>";
     }
+
 } else {
-    echo "Nuk keni dërguar të dhëna.";
+    echo "Nuk keni dërguar të dhëna.";
 }
 ?>
